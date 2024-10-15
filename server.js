c119a255d90b438ec4d4a1d3fed8af49ea3615f1
noblekuz <noblekuz@gmail.com>
2024-10-15 14:25:12 +0200

    Connection to Mongo Server Setup

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+
+
+const express = require('express');
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
 // fileName : server.js 
 // Example using the http module
-const express = require('express');
 const app = express();
 const router = express.Router();
+app.use(cors());
+app.use(express.json());
+
+
 
 // Making Routes
 app.get('/', (req, res) => {
@@ -17,20 +27,30 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-const url = //"mongodb://localhost:27017"; 
-mongoose.connect(url, { useNewUrlParser: true });
-const con = mongoose.connection;
+const uri = "mongodb+srv://yaokuz:euYrCqua7mQtA0kH@clusterclientbuyer.uyo2s.mongodb.net/?retryWrites=true&w=majority&appName=ClusterClientBuyer";
+const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
 
 
-app.use(express.json());
 
-try {
-    con.on('open', () => {
-        console.log('Connected to the database');
-    })
-} catch (error) {
-    console.log("Error: " + error);
-}
+  async function run() {
+    try {
+      // Connect the client to the server	(optional starting in v4.7)
+      await client.connect();
+      // Send a ping to confirm a successful connection
+      await client.db("admin").command({ ping: 1 });
+      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
 
 // Include route files
 const usersRoute = require('./routes/api/usersRoute');
